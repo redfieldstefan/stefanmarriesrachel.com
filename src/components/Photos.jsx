5bfc2cdd101f69ff929c40900b3f9880eaaee001
Stefan Redfield <redfield.stefan@gmail.com>
2019-10-19 13:23:51 -0700

    make photos for photo page directory

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Page from './Page';
 import '../styles/photos.css';
 
-const context = require.context('../photos', true, /\.(jpg)$/i);
+const context = require.context('../photos/photos', true, /\.(jpg)$/i);
 const sources = context.keys().map(filename => {
 	return filename.split('./')[1];
 }).sort(() => Math.random() - 0.5);
@@ -11,7 +11,7 @@ const Photos = () => {
 	return (
 		<Page className="Photos">
 			{
-				sources.map(path => <img className="photo" src={require(`../photos/${path}`)} />)
+				sources.map(path => <img className="photo" src={require(`../photos/photos/${path}`)} />)
 			}
 		</Page>
 	);
